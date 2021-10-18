d9f5d51cc4845a9138108b022c528093a5ed8071
Nicolas Capelier <capelier.nicolas@gmail.com>
2021-10-18 09:14:28 +0200

    Added LD tool architecture

---
+++ Hold the tower/Assets/Scripts/LevelElements/ThemeManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DisallowMultipleComponent]
+public class ThemeManager : MonoBehaviour
+{
+
+}
