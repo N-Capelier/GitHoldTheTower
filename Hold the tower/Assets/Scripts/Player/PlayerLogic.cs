f99422106043682bd256fe1d40992b18139f853f
Oses Jean-Baptiste <jb.os@hotmail.fr>
2021-11-03 17:17:49 +0100

    Fixed Jump

--- Hold the tower/Assets/Scripts/Player/PlayerLogic.cs
+++ Hold the tower/Assets/Scripts/Player/PlayerLogic.cs
@@ -126,7 +126,6 @@ public class PlayerLogic : NetworkBehaviour
             }
             else
             {
-                selfMovement.NoGravity();
                 if (!isJumping)
                 {
                     selfMovement.NoGravity();
