1550479f5f9fa8547dbf435ec1fb52b02ecbd1b4
William <w.schmitt@rubika-edu.com>
2021-11-23 15:16:41 +0100

    Added Sound Manager

---
+++ Hold the tower/Assets/Scripts/Audio/SoundReference.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundReference : MonoBehaviour
+{
+    public string name;
+
+    public int ID;
+
+    public Sound sound;
+
+    public AudioSource audioSource;
+
+
+    public void ApplySoundToAudioSource(Sound sound, bool loop, AudioMixerGroup mixer)
+    {
+        audioSource.clip = sound.clip;
+        audioSource.volume = sound.volume;
+        audioSource.loop = loop;
+        audioSource.outputAudioMixerGroup = mixer;
+    }
+
+
+
+
+}
